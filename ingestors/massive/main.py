d84c5e06ad9360e0676fbc43679833fa93713f22
Josh Peck <jmp@joshpeck.org>
2026-01-25 12:29:34 +0800

    Refactoring massive ingesor to clean up the (working) mess

--- ingestors/massive/main.py
+++ ingestors/massive/main.py
@@ -2,26 +2,46 @@ import os
 import tempfile
 from datetime import date, datetime
 from typing import BinaryIO
-from dataclasses import dataclass
+import logging
 import boto3
 import botocore
 from google.cloud import storage
+import pandas as pd
 
-report_aggregations_map = {
+# Configure global logging settings
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+SOURCE_BUCKET_NAME = "flatfiles"
+REPORT_AGGREGATIONS_MAP = {
     'daily': 'day_aggs_v1',
 }
 
-@dataclass
-class MassiveSourceObject:
-    series_id: str
-    resolution: str
-    report_date: date
+def report_aggregation_stub(resolution: string):
+    return REPORT_AGGREGATIONS_MAP[resolution]
 
-    # Example path: 's3://flatfiles/us_stocks_sip/day_aggs_v1/2026/01/2026-01-22.csv.gz'
-    def getObjectKey(self):
-        object_key = f"{self.series_id}/{self.resolution}/{self.report_date.year}/{self.report_date.month:02}/{self.report_date.year}-{self.report_date.month:02}-{self.report_date.day:02}.csv.gz"
-        return object_key
+def massive_object_key(series_id: str, resolution: str, report_date: date):
+    object_key = f"{series_id}/{resolution}/{report_date.year}/{report_date.month:02}/{report_date.year}-{report_date.month:02}-{report_date.day:02}.csv.gz"
+    return object_key
 
+def gcp_blob_path(series_id: str, resolution: str, report_date: date, format: str):
+    today: str = datetime.now().date().strftime("%Y-%m-%d")
+    return f"massive/{series_id}/frequency={resolution}/ingest_date={today}/{report_date.year}-{report_date.month:02}-{report_date.day:02}{format}"
+
+def gcp_landing_zone_upload(client: storage.Client, tmpfile: tempfile.TemporaryFile, bucket_name: str, blob_path: str):
+    tmpfile.seek(0)
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(blob_path)
+    return blob.upload_from_file(tmpfile)
+
+def gcp_bronze_upload(client: storage.Client, tmpfile: tempfile.TemporaryFile, bucket_name: str, blob_path: str):
+    tmpfile.seek(0)
+    df = pd.read_csv(tmpfile, compression='gzip')
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(blob_path)
+    return blob.upload_from_string(
+        df.to_parquet(index=False), 
+        content_type='application/octet-stream'
+    )
 
 def run_ingestion():
     # create GCP connection
@@ -53,35 +73,19 @@ def run_ingestion():
     )
 
     # 1. Download flat file via S3
-    bucket_name = "flatfiles"
-    mso = MassiveSourceObject(series_id, report_aggregations_map[resolution], report_date)
-
-    # object_key = f"flatfiles/{series_id}/{report_aggregations_map[resolution]}/{report_date.year}/{report_date.month}/{report_date.year}-{report_date.month}-{report_date.day}.csv.gz"
+    source_object_key = massive_object_key(series_id, report_aggregation_stub(resolution), report_date)
     with tempfile.TemporaryFile() as tmpfile:
         try:
-            s3.download_fileobj(bucket_name, mso.getObjectKey(), tmpfile)
-            print("-- MASSIVE SOURCE FILE DOWNLOAD: COMPLETE")
-            # gcp_upload(storage_client, landing_zone_bucket_name, )
+            s3.download_fileobj(SOURCE_BUCKET_NAME, source_object_key, tmpfile)
+            logging.info(f"Massive source file downloaded: {SOURCE_BUCKET_NAME}/{source_object_key}")
+            landing_zone_blob_path = gcp_blob_path(series_id, resolution, today, '.csv.gz')
+            gcp_landing_zone_upload(storage_client, tmpfile, landing_zone_bucket_name, landing_zone_blob_path)
+            logging.info(f"Landing Zone file uploaded: {landing_zone_bucket_name}/{landing_zone_blob_path}")
+            bronze_blob_path = gcp_blob_path(series_id, resolution, today, '.parquet')
+            gcp_bronze_upload(storage_client, tmpfile, bronze_bucket_name, bronze_blob_path)
+            logging.info(f"Bronze file uploaded: {bronze_bucket_name}/{bronze_blob_path}")
         except Exception as e:
-            print(f'Error downloading file: {type(e).__name__}:{e}')
-
-        # XXX: refactor into function or class to simplify
-        # 2. Write original file to landing zone without ANY modification for traceability later
-        tmpfile.seek(0)
-        landing_zone_blob_path = f"massive/{series_id}/frequency={resolution}/ingest_date={today}/{report_date.year}-{report_date.month:02}-{report_date.day:02}.csv.gz"
-        bucket = storage_client.bucket(landing_zone_bucket_name)
-        blob = bucket.blob(landing_zone_blob_path)
-        blob.upload_from_file(tmpfile)
-        print(f"Successfully wrote {series_id} to {landing_zone_blob_path}")
-
-        # XXX: refactor into function or class to simplify
-        # 2. Write original file to landing zone without ANY modification for traceability later
-        tmpfile.seek(0)
-        bronze_blob_path = f"massive/{series_id}/frequency={resolution}/ingest_date={today}/{report_date.year}-{report_date.month:02}-{report_date.day:02}.parquet"
-        bucket = storage_client.bucket(bronze_bucket_name)
-        blob = bucket.blob(bronze_blob_path)
-        blob.upload_from_file(tmpfile)
-        print(f"Successfully wrote {series_id} to {bronze_blob_path}")
+            print(f'Error ingesting file ({source_object_key}): {type(e).__name__}:{e}')
 
 if __name__ == "__main__":
     run_ingestion()
